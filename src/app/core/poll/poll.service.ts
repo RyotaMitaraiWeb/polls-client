d4e7cdd185d59073de86225e11d3a3660f49d1ec
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-12-11 12:07:58 +0200

    add poll service

---
+++ src/app/core/poll/poll.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPoll, IPollSubmission, IPollSuccessfulAction } from 'src/app/interfaces';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PollService {
+
+    constructor(private readonly http: HttpClient) { }
+
+    url: string = 'http://localhost:3000/poll';
+
+    getPoll(id: number) {
+        return this.http.get<IPoll>(this.url + '/' + id, {
+            headers: {
+                'Authorization': localStorage.getItem('token') as string || '',
+            }
+        });
+    }
+
+    createPoll(poll: IPollSubmission) {
+        return this.http.post<IPollSuccessfulAction>(this.url + '/create', poll, {
+            headers: {
+                'Authorization': localStorage.getItem('token') as string || '',
+            }
+        });
+    }
+
+    editPoll(id: number, title: string) {
+        return this.http.patch<IPollSuccessfulAction>(`${this.url}/${id}/edit`, { title }, {
+            headers: {
+                'Authorization': localStorage.getItem('token') as string || '',
+            }
+        });
+    }
+
+    deletePoll(id: number) {
+        return this.http.delete<{}>(`${this.url}/${id}/delete`, {
+            headers: {
+                'Authorization': localStorage.getItem('token') as string || '',
+            }
+        });
+    }
+}
