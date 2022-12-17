6dce1f7fc3018d98b3db14352f2aa027352ef8ba
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-12-17 17:21:44 +0200

    add tests for shorten pipe

--- src/app/shared/shorten/shorten.pipe.spec.ts
+++ src/app/shared/shorten/shorten.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { ShortenPipe } from './shorten.pipe';
 
 describe('ShortenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortenPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: ShortenPipe;
+    beforeEach(() => {
+        pipe = new ShortenPipe();
+    })
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('Returns the same string if it is less than the given amount of characters', () => {
+        expect(pipe.transform('a', 50)).toBe('a');
+    });
+
+    it('Returns the same string if the amount of characters matches the given amount', () => {
+        expect(pipe.transform('a', 1)).toBe('a');
+    });
+
+    it('Transforms string if the amount of characters exceeds the given amount', () => {
+        expect(pipe.transform('aa', 1)).toBe('a...');
+    })
 });
